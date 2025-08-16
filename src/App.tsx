f9fc6fcdf8dd557dbd0802eec4cf0ae4e37a4c76
Lucas Fonseca <lucasfonsecaeditor@gmail.com>
2025-08-16 12:19:24 -0300

    feat: update app router

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,15 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 
 import { Toaster } from 'sonner';
-import { IndexPage } from './pages';
-import { AdminAdsPage } from './pages/admin/admin-ads-page';
-import { AdminDashboardPage } from './pages/admin/admin-dashboard-page';
-import { AdminHomePage } from './pages/admin/admin-home-page';
-import AdminLoginPage from './pages/admin/admin-login-page';
-import { PrivacyPolicyPage } from './pages/privacy-policy';
-import { TermsPage } from './pages/terms-page';
-import { ProtectedRoute } from './routes/protected-route';
+import { router } from './router';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<IndexPage />} />
-        <Route path="/admin/login" element={<AdminLoginPage />} />
+    <>
+      <RouterProvider router={router} />
 
-        <Route path="/terms" element={<TermsPage />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
-
-        <Route
-          path="/admin"
-          element={
-            <ProtectedRoute>
-              <AdminDashboardPage />
-            </ProtectedRoute>
-          }
-        >
-          <Route path="home" element={<AdminHomePage />} />
-          <Route path="ads" element={<AdminAdsPage />} />
-        </Route>
-
-        {/* <Route path="*" element={<NotFoundPage />} /> */}
-      </Routes>
       <Toaster position="top-right" richColors />
-    </Router>
+    </>
   );
 }
 
