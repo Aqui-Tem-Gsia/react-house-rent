f506d8f4703c0987b270eb46889bad3486ad90ae
Lucas Fonseca <lucasfonsecaeditor@gmail.com>
2025-08-16 12:19:24 -0300

    feat: update app router

---
+++ src/router.tsx
@@ -0,0 +1,48 @@
+import { createBrowserRouter, redirect } from 'react-router-dom';
+import { AdminAdsPage } from './pages/admin/admin-ads-page';
+import { AdminDashboardPage } from './pages/admin/admin-dashboard-page';
+import { AdminHomePage } from './pages/admin/admin-home-page';
+import AdminLoginPage from './pages/admin/admin-login-page';
+import { PrivacyPolicyPage } from './pages/privacy-policy';
+import { TermsPage } from './pages/terms-page';
+import { ProtectedRoute } from './routes/protected-route';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <></>,
+    loader: () => {
+      return redirect('/admin/login');
+    },
+  },
+  {
+    path: '/terms',
+    element: <TermsPage />,
+  },
+  {
+    path: '/privacy-policy',
+    element: <PrivacyPolicyPage />,
+  },
+  {
+    path: '/admin/login',
+    element: <AdminLoginPage />,
+  },
+  {
+    path: '/admin',
+    element: (
+      <ProtectedRoute>
+        <AdminDashboardPage />
+      </ProtectedRoute>
+    ),
+    children: [
+      {
+        path: 'home',
+        element: <AdminHomePage />,
+      },
+      {
+        path: 'ads',
+        element: <AdminAdsPage />,
+      },
+    ],
+  },
+]);
