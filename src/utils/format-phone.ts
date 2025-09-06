8963a43a64c9afada93b23bbbd6e3851e2305cca
gustavodev200 <gustavolage200@gmail.com>
2025-09-06 14:24:12 -0300

    fix: add early return in formatPhone

--- src/utils/format-phone.ts
+++ src/utils/format-phone.ts
@@ -1,3 +1,4 @@
-export const formatPhone = (phone: string) => {
+export const formatPhone = (phone?: string | null) => {
+  if (!phone) return "-";
   return phone.replace(/(\d{2})(\d{4,5})(\d{4})/, "($1) $2-$3");
 };
